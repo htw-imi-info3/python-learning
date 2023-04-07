dd61928ee5668e0489d20742f8d5442c3808a0b7
Barne Kleinen <barne.kleinen@htw-berlin.de>
2023-04-07 12:34:33 +0200

    add example test file

--- about/test_conditionals.py
+++ about/test_conditionals.py
@@ -1,8 +1,17 @@
 import unittest
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+class TestConditional(unittest.TestCase):
+    def test_if(self):
+        result = "none"
+        a = 6
+        if a == 5:
+            result = 5
+        else:
+            result = "not 5"
+
+        self.assertEqual("not 5", result, "the else case should be used")
+
 
 if __name__ == '__main__':
     unittest.main()
