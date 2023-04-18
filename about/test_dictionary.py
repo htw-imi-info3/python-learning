7edbc0c1bb830ec028e1831fe5f77b98829b6fe1
Jonas Trenkler <jonas.trenkler@posteo.de>
2023-04-18 12:09:01 +0200

    dict: add fixture and comments

--- about/test_dictionary.py
+++ about/test_dictionary.py
@@ -1,16 +1,19 @@
 import unittest
 
 class DictionaryTestCase(unittest.TestCase):
-    def test_dictionary_definition(self):
-
-        colors = {"red"   : 0xff0000,
-                  "green" : 0x00ff00,
-                  "blue"  : 0x0000ff
+    def setUp(self):
+        """This is run before every test"""
+        self.colors = {"red"   : 0xff0000,
+                       "green" : 0x00ff00,
+                       "blue"  : 0x0000ff
         }
-        
-        colors_2 = { "red" : 0xFF0000, "green" : 0x00FF00, "blue" : 0x0000FF }
 
-        self.assertDictEqual(colors, colors_2, "The dictionaries should contain identical entries")
+    def test_dictionary_definition(self):
+        """ Dictionary syntax does not rely on whitespace.
+        It uses braces, colons for key : value and commas between entries."""
+
+        colors_2 = { "red" : 0xFF0000, "green" : 0x00FF00, "blue" : 0x0000FF }
+        self.assertDictEqual(self.colors, colors_2, "The dictionaries should contain identical entries")
 
 if __name__ == "__main__":
     unittest.main()
