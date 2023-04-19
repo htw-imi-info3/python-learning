e8054e7e324d9694b8f8a450671e2a6ebd9b3540
mar7-n <116806411+mar7-n@users.noreply.github.com>
2023-04-19 17:04:01 +0200

    first commit forloop

---
+++ about/test_forloop.py
@@ -0,0 +1,20 @@
+import unittest
+
+class TestForLoop(unittest.TestCase):
+
+    def test_1(self):
+        list1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        list2 = []
+        for x in range(10):
+            list2.append(x)
+        self.assertEqual(list1,list2)
+
+    def test_2(self):
+        list1 = []
+        list2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        for x in range(10):
+            list2.pop()
+        self.assertEqual(list1,list2)
+
+if __name__ == '__main__':
+    unittest.main()
