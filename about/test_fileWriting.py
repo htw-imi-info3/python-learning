6a530007479ebb089678addee4b10763a64b2f9b
mantayjon <Jonas.Mantay@Student.HTW-Berlin.de>
2023-04-21 16:59:43 +0200

    Update test_fileWriting.py

--- about/test_fileWriting.py
+++ about/test_fileWriting.py
@@ -1,13 +1,20 @@
 import unittest
+import os
 
 
 class MyTestCase(unittest.TestCase):
-    def test_open_file(self):
-       # self.assertEqual(True, False)  # add assertion here
-""
-    def test_closing_file(self):
+    def test_fileWriter(self):
+        # creating a temporary File and writing in it
+        with open('testFile.txt', 'w') as temp_file:
+            temp_file.write("this is a test")
 
-    def test_write_in_file(self):
+        # looking if the written text is correct
+        with open(temp_file.name, 'r') as file:
+            self.assertIn("this is a test", file.read())
+
+        # deleting the temporary file
+        os.remove(temp_file.name)
+
 
 if __name__ == '__main__':
     unittest.main()
