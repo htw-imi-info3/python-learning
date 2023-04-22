733a82602418d3b9af1d442e50dd7e110382e093
abductedRhino <avocadorable@mailbox.org>
2023-04-22 17:49:14 +0200

    refactor test_isclass() and setUp()

--- about/test_class-definition.py
+++ about/test_class-definition.py
@@ -6,26 +6,17 @@ class ClassDefinitionTestCase(unittest.TestCase):
 
     def setUp(self):
         self.test_cases = {
-            'py_class_list': [
-                int,
-                float,
-                complex
-            ],
-            'py_non_class_list': [
-                True,
-                False,
-                None
-            ]
+            int: True,
+            float: True,
+            complex: True,
+            True: False,
+            False: False,
+            None: False
         }
-        self.example_variable = int
 
     def test_isclass(self):
-        for built_in_class in self.test_cases['py_class_list']:
-            self.assertTrue(inspect.isclass(built_in_class))
-        for built_in_class in self.test_cases['py_non_class_list']:
-            self.assertFalse(inspect.isclass(built_in_class))
-        self.assertTrue(inspect.isclass(self.example_variable),
-                        msg='"' + str(self.example_variable) + '" is not a class.')
+        for key in self.test_cases:
+            self.assertEqual(inspect.isclass(key), self.test_cases[key])
 
 
 if __name__ == '__main__':
