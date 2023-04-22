cb426d0534cdeefbc4c0093884914a1f39485263
abductedRhino <avocadorable@mailbox.org>
2023-04-22 17:38:02 +0200

    add setUp()

--- about/test_class-definition.py
+++ about/test_class-definition.py
@@ -2,11 +2,30 @@ import unittest
 import inspect
 
 
-
 class ClassDefinitionTestCase(unittest.TestCase):
 
+    def setUp(self):
+        self.test_cases = {
+            'py_class_list': [
+                int,
+                float,
+                complex
+            ],
+            'py_non_class_list': [
+                True,
+                False,
+                None
+            ]
+        }
+        self.example_variable = int
+
     def test_isclass(self):
-        self.assertTrue(inspect.isclass())
+        for built_in_class in self.test_cases['py_class_list']:
+            self.assertTrue(inspect.isclass(built_in_class))
+        for built_in_class in self.test_cases['py_non_class_list']:
+            self.assertFalse(inspect.isclass(built_in_class))
+        self.assertTrue(inspect.isclass(self.example_variable),
+                        msg='"' + str(self.example_variable) + '" is not a class.')
 
 
 if __name__ == '__main__':
