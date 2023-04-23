24ec05256e9edaabeae5dcab425505fd2c1e8e5f
abductedRhino <avocadorable@mailbox.org>
2023-04-23 22:29:00 +0200

    Related to #21

--- about/test_lists.py
+++ about/test_lists.py
@@ -2,15 +2,19 @@ import unittest
 
 my_shoppinglist = []
 
-def add_to_shoppingList(addition):
+
+def add_to_shoppinglist(addition):
     my_shoppinglist.append(addition)
 
 
 class TestList(unittest.TestCase):
 
-    def test1(self):
-        add_to_shoppingList("Pineapple")
-        self.assertEqual(my_shoppinglist, ['Pineapple'], "ERROR")
+    def setUp(self):
+        my_shoppinglist.clear()
+
+    def test_append(self):
+        add_to_shoppinglist('Pineapple')
+        self.assertEqual(my_shoppinglist, ['Pineapple'], 'ERROR')
 
 
 if __name__ == '__main__':
