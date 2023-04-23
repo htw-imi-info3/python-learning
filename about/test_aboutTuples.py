97587374ed8cd1a0eebf7168a0933a327d7977ed
Jonas Trenkler <jonas.trenkler@posteo.de>
2023-04-23 13:09:21 +0200

    tuples: make variables local

--- about/test_aboutTuples.py
+++ about/test_aboutTuples.py
@@ -1,10 +1,5 @@
 import unittest
 
-myTuple = ("apple", "banana", "cherry")
-noTuple = ("apple")
-oneTuple = ("apple",)
-mixedTuple = ("abc", 34, True, 40, "male")
-
 """
 - Tuples are used to store multiple items in a single variable.
 - Tuple items are ordered, unchangeable, and allow duplicate values.
@@ -13,18 +8,22 @@ mixedTuple = ("abc", 34, True, 40, "male")
 
 class TestAboutTuple(unittest.TestCase):
     def test1(self):
+        myTuple = ("apple", "banana", "cherry")
         length = len(myTuple)
         self.assertEqual(length, 3, "Has three Items")
 
     def test2(self):
+        noTuple = ("apple")
         elType = type(noTuple)
         self.assertEqual(elType, str, "Missing Comma - No Tuple")
 
     def test3(self):
+        oneTuple = ("apple",)
         elType = type(oneTuple)
         self.assertEqual(elType, tuple, "Comma present - Tuple")
 
     def test4(self):
+        mixedTuple = ("abc", 34, True, 40, "male")
         elType = type(mixedTuple)
         self.assertEqual(elType, tuple, "Tuple")
 
