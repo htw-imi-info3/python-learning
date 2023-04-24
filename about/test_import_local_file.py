e47decb9b8ea003b061d38185d2b3914334b2da5
leunami420 <marconich311@gmail.com>
2023-04-24 18:39:20 +0200

    adapted the test_import_local_file to always select a random test parameter (issue #23)

--- about/test_import_local_file.py
+++ about/test_import_local_file.py
@@ -1,13 +1,17 @@
 import unittest
 import testimport
+import random
 
 
 class TestImportLocalFile(unittest.TestCase):
+    names = ['Mathieu', 'Imanuel', 'Timo', 'Sascha', 'Chris', 'Jesus']
 
     def test_import_local_file(self):
-        name = "Mathieu"
+        print("Running test_import_local_file...")
+        name = random.choice(self.names)
         greeting = testimport.greet(name)
-        self.assertEqual(greeting, "Hello, Mathieu!")
+        print(greeting)
+        self.assertEqual(greeting, "Hello, " + name + "!")
 
 
 if __name__ == '__main__':
