b8e789f54209301a9a28f9e9115ba4a869cb02b2
ultratynoz <tynoz@web.de>
2023-04-25 14:00:53 +0200

    lines 20/21 were indented one tab too much, but test works perfectly

--- about/test_filereading.py
+++ about/test_filereading.py
@@ -1,19 +1,21 @@
 import unittest
 
+
 class TestFile(unittest.TestCase):
-    def testfilereading(self):
+    def test_filereading(self):
         expected_output = "AusgabeAusgabe"
         try:
             with open("lab2reading.txt", "r") as f:
                 content = f.read().strip()
             if content == expected_output:
-                print("Test passed: Content matches the Output.")
+                print("Test passed: Content matches the output.")
             else:
-                print("Test failed: Content doesnt match the output.")
+                print("Test failed: Content doesn't match the output.")
                 self.fail()
         except FileNotFoundError:
             print("Test failed: File not found.")
             self.fail()
 
-    if __name__ == '__main__':
-        unittest.main()
+
+if __name__ == '__main__':
+    unittest.main()
